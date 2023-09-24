7373dfcde392166551df689bb2fb9ec69182eda0
kmc369 <keishacoleman710@gmail.com>
2023-09-23 23:15:02 -0400

    Delete started

---
+++ react-app/src/components/DeleteNote/index.js
@@ -0,0 +1,16 @@
+import "./DeleteNote.css"
+import React, { useState ,useEffect} from "react";
+import {useParams } from 'react-router-dom';
+import { useDispatch,useSelector } from "react-redux";
+import * as NoteActions from '../../store/note'
+
+function DeleteNote() {
+
+return (
+    <>
+    
+    </>
+)
+
+}
+export default DeleteNote
